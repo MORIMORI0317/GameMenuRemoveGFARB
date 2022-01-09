b4bffa00167a815c3028e4fdd0ff9f852525a96b
MORIMORI0317 <morimori.0317@outlook.jp>
2022-01-09 20:34:23 +0900

    fix server list FML incompatibility notification

--- src/main/java/net/morimori/gmrg/GameMenuRemoveGFARB.java
+++ src/main/java/net/morimori/gmrg/GameMenuRemoveGFARB.java
@@ -1,14 +1,19 @@
 package net.morimori.gmrg;
 
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 
 @Mod("gamemenuremovegfarb")
 public class GameMenuRemoveGFARB {
 
     public GameMenuRemoveGFARB() {
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
     }
 
